e02840e3ea9b95d07239756c03bb9621ab047e25
Bowie <marketing@letscode.com.br>
2024-07-02 16:49:43 -0300

    Atualizado estilo e tela de resultado.

--- app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -12,33 +13,30 @@ const val KEY_RESULT_IMC = "ResultActivity.KEY_IMC"
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_result)
 
 
             val result = intent.getFloatExtra(KEY_RESULT_IMC,0f)
-
             val tvResult = findViewById<TextView>(R.id.tv_result)
             val tvClassificacao = findViewById<TextView>(R.id.tv_classificacao)
-
-
             tvResult.text = result.toString()
 
-             val classificacao: String = if(result <= 18.5f) {
-                 "ABAIXO DO PESO"
-             } else if ( result > 18.5f && result <= 24.9f){
-                 "PESO NORMAL"
-             } else if (result >= 25f && result <= 29.9f){
-                 "SOBREPESO"
-             } else if (result >= 30f && result <=34.9f){
-                 "OBESIDADE CLASSE 1"
-             } else if (result >= 35 && result <= 39.9f) {
-                 "OBESIDADE CLASSE 2"
-             }
-             else {
-                 "OBESIDADE CLASSE 3"
+             var (classificacao: String, color) = when {
+                 result <= 18.5f -> {
+                     "Subpeso" to R.color.Subpeso
+                 }
+
+                 result > 18.5f && result <= 25.0f -> {
+                     "Normal" to R.color.Normal
+                 }
+
+                 else -> {
+                     "Sobrepeso" to R.color.Sobrepeso
+
+                 }
              }
 
+            tvClassificacao.setTextColor(ContextCompat.getColor(this, color))
             tvClassificacao.text = classificacao
         }
     }
